b3a7063aebe59bac1d2e5f1b0e77418c69d0d48c
saurabh kabra <saurabh.rk0012@gmail.com>
2025-07-31 17:09:08 +0530

    Final update for Streamlit deployment: env removed, os.environ used

--- question_generator.py
+++ question_generator.py
@@ -1,11 +1,8 @@
 # question_generator.py
 import google.generativeai as genai
-from dotenv import load_dotenv
 import os
 
-load_dotenv()
-
-api_key = os.getenv("GEMINI_API_KEY")
+api_key = os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=api_key)
 
 model = genai.GenerativeModel("gemini-2.0-flash")
